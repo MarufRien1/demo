bdc98b4e5efd39f930de24fd5be2933721c58f32
Md. Maruf Rien <maruf14222@gmail.com>
2023-07-30 21:35:58 +0600

    add location page

--- src/app/location/page.jsx
+++ src/app/location/page.jsx
@@ -1,10 +1,42 @@
+import ActionButtonRow from '@/components/ActionButtonRow';
+import BackBtn from '@/components/BackBtn';
+import { InfoCard } from '@/components/InfoCard';
 import TopHeader from '@/components/TopHeader';
+import Image from 'next/image';
 import React from 'react';
+import { MdLocationOn } from 'react-icons/md';
 
 const Location = () => {
   return (
     <div>
       <TopHeader />
+      <div className="mt-10 ml-16">
+        <BackBtn />
+        <InfoCard
+          className
+          title="Where's your place located?"
+          body="In this step, we'll ask you which type of stays you have and if guests will book the entire place or just a room."
+        />
+
+        <div className=" w-fit flex   border-2 border-textColor  rounded-full  text-2xl p-3 text-textColor pr-[300px] m-5 ml-0 ">
+          <MdLocationOn size={30} className="ml-1" />
+          <form className="ml-3">
+            <input
+              className="outline-none"
+              type="text"
+              name="location"
+              id="locationInput"
+              placeholder="Enter location"
+            />
+          </form>
+        </div>
+
+        <Image src={'/map.png'} width={1230} height={447} sizes="100vw" />
+      </div>
+
+      <div className=" mr-10 mt-5 ">
+        <ActionButtonRow link="/room" />
+      </div>
     </div>
   );
 };
--- src/components/BackBtn.jsx
+++ src/components/BackBtn.jsx
@@ -3,8 +3,8 @@ import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 const BackBtn = () => {
   return (
-    <button className=" inline-block rounded-full bg-gray-200  mb-10">
-      <AiOutlineArrowLeft size={38} className="p-2" color="#9CA3AF" />
+    <button className=" inline-block rounded-full bg-[#F2F2F7]  mb-10">
+      <AiOutlineArrowLeft size={38} className="p-2" color="#ABABBB" />
     </button>
   );
 };
