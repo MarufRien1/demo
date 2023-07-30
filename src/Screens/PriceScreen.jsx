1fdf9991149609dbb79471aa5602f959d656a88d
Md. Maruf Rien <maruf14222@gmail.com>
2023-07-30 15:22:35 +0600

    fix: typo

--- src/Screens/PriceScreen.jsx
+++ src/Screens/PriceScreen.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 import Header from '../components/Header';
 import { InfoCard } from '../components/InfoCard';
 import BackBtn from '../components/BackBtn';
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { RxAvatar } from 'react-icons/rx';
-import BackBtn from './BackBtn';
+
 const Header = () => {
   return (
     <>
